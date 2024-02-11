478705f41e5f721a78590a817f171a63360fd46d
ddipayan00 <ddipayan00@gmail.com>
2024-02-11 12:21:18 +0530

    firebaseCred.json added

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,12 +3,10 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import firebaseConfigJson from "../Credentials/firebaseCred.json" assert { type: 'json' };
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {};
-
+const firebaseConfig = firebaseConfigJson;
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
